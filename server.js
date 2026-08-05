2d3779ad192736f62c4f796a68e00dd85f2c14ea
meenabhavna2024-glitch <meenabhavna2024@gmail.com>
2026-08-05 20:09:15 +0530

    Fix Render port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ io.on("connection", (socket) => {
 
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, "0.0.0.0", () => {
     console.log(`Chat Server Started on port ${PORT}`);
